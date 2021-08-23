d191250d065935a8a6ba48ffd464467fb88deac5
Johnathan-Yan <47734393+Johnathan-Yan@users.noreply.github.com>
2021-08-23 21:02:17 +1000

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,3 @@
 document.addEventListener("Test", function(event) {
-    // Your code to run since DOM is loaded and ready
-    let p1 = document.getElementById("banner");
-    p1.textContent = "Search for scolar articles. Currently serving TEST";
+    alert(1);
 });
